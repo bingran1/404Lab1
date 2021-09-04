3be909b65669c8e25f18eda2f2a844ef841983ab
bingran1 <58350943+bingran1@users.noreply.github.com>
2021-09-04 17:14:42 -0600

    Add files via upload

--- python_script.py
+++ python_script.py
@@ -1,4 +1,9 @@
 import requests
 print(requests.__version__)
-r = requests.get('http://google.com/')
-print(r.text)
+r1 = requests.get('http://google.com/')
+print("For Google Homepage: ")
+print(r1.text)
+
+r2 = requests.get('https://raw.githubusercontent.com/bingran1/404Lab1/main/python_script.py')
+print("For my python script: ")
+print(r2.text)
